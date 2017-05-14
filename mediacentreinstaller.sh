239e089e57a503c756b446e105c4551bc488807f
thelastparadox <thelastparadox@gmail.com>
2017-05-14 19:42:04 +0100

    Changes to logging

--- mediacentreinstaller.sh
+++ mediacentreinstaller.sh
@@ -22,6 +22,7 @@ DEFAULT_MOVIES_LOCATION="/NAS/video/movies"
 INSTALL_COMMAND_SONARR="sudo docker run --restart=always -d --name=sonarr -p 8989:8989 -e PUID=1000 -e PGID=1000 -v /dev/rtc:/dev/rtc:ro -e TZ="Europe/London" -v "$DOCKER_LOCATION"/sonarr/config:/config -v "$TVSHOWS_LOCATION":/tv -v "$DOWNLOADS_LOCATION":/downloads -v "$DOWNLOADS_LOCATION":/NAS/downloads -v "$TVSHOWS_LOCATION":"$TVSHOWS_LOCATION" linuxserver/sonarr $QUIET_MODE"
 CONTINUE=true
 LOG_FILE="$(dirname $0)/${THEME_NAME_SHORT_NAME,,}-log-$(date +%Y-%m-%d).log"
+LOGFILE_DATEFORMAT="+%Y-%m-%d:%H:%M:%S"
 
 # Setup Quiet Mode
 if [[ CONFIG_QUIET_MODE == true ]]; then
@@ -34,7 +35,7 @@ fi
 whiptail --title "$THEME_NAME_LONG_NAME $VERSION" --msgbox "Welcome to the $THEME_NAME_LONG_NAME. This guided installer will download, install and configure any software you choose by using the CE edition of Docker. Please click OK to continue." 12 78
 
 # Beginning Install
-echo -e "$(date +%Y-%m-%d:%H:%M:%S) $THEME_BOFLINE Beginning installation...\n" >> $LOG_FILE
+echo -e "$(date +%Y-%m-%d:%H:%M:%S) $THEME_BOFLINE Beginning installation..." >> $LOG_FILE
 
 # Check to see if platform is Ubuntu
 OS_1="$(gawk -F= '/^NAME/{print $2}' /etc/os-release)"
@@ -43,9 +44,9 @@ OS="${OS_1//\"}"
 if [[ OS -ne "Ubuntu" ]]; then
   whiptail --title "ERROR" --msgbox "Unfortunately this installer is only configured for Ubuntu at the moment." 12 78
   CONTINUE=false
-  echo -e "$(date +%Y-%m-%d:%H:%M:%S) $THEME_BOFLINE OS is not compatible with installer ($OS)\n" >> $LOG_FILE
+  echo -e "$(date $LOGFILE_DATEFORMAT) $THEME_BOFLINE OS is not compatible with installer ($OS)" >> $LOG_FILE
 else
-  echo -e "$(date +%Y-%m-%d:%H:%M:%S) $THEME_BOFLINE OS has been confirmed as Ubuntu\n" >> $LOG_FILE
+  echo -e "$(date $LOGFILE_DATEFORMAT) $THEME_BOFLINE OS confirmed as Ubuntu" >> $LOG_FILE
 fi
 
 # Select INSTALL_CHOICES to Install
@@ -58,16 +59,17 @@ if [[ $CONTINUE == true ]]; then
   "Plex" "Media Centre" ON \
   "Jackett" "Torrent Indexer" ON \
   "Cardigann" "Torrent Indexer (no longer supported)" ON \
-  "Portainer" "Dahsboard for managing INSTALL_CHOICES" ON \
+  "Portainer" "Dahsboard for managing INSTALL_CHOICES" OFF \
   3>&1 1>&2 2>&3)
-  echo -e "$(date +%Y-%m-%d:%H:%M:%S) $THEME_BOFLINE Install choices are: $INSTALL_CHOICES\n" >> $LOG_FILE
+  INSTALL_CHOICES="${INSTALL_CHOICES//\"/}"
+  echo -e "$(date $LOGFILE_DATEFORMAT) $THEME_BOFLINE Install choices are: $INSTALL_CHOICES" >> $LOG_FILE
 fi
 
 if [[ $CONTINUE == true && $INSTALL_CHOICES != "" ]]; then
   if (whiptail --title "Usage of VPN" --yesno "Would you like to run all traffic via a VPN?." 8 78); then
     VPN="yes"
-    INSTALL_CHOICES="${INSTALL_CHOICES} VPN"
-    echo -e "$(date +%Y-%m-%d:%H:%M:%S) $THEME_BOFLINE VPN use: yes\n" >> $LOG_FILE
+    INSTALL_CHOICES="${INSTALL_CHOICES} VPN Proxy"
+    echo -e "$(date $LOGFILE_DATEFORMAT) $THEME_BOFLINE Use the VPN: yes" >> $LOG_FILE
   else
     VPN="no"
   fi
@@ -78,9 +80,9 @@ if [[ $CONTINUE == true && $INSTALL_CHOICES != "" ]]; then
   exitstatus=$?
   if [[ "$exitstatus" = 1 ]]; then
     CONTINUE=false
-    echo -e "$(date +%Y-%m-%d:%H:%M:%S) $THEME_BOFLINE User did not confirm Docker config location\n" >> $LOG_FILE
+    echo -e "$(date $LOGFILE_DATEFORMAT) $THEME_BOFLINE User did not confirm Docker config location" >> $LOG_FILE
   else
-    echo -e "$(date +%Y-%m-%d:%H:%M:%S) $THEME_BOFLINE Docker Config Location\n" >> $LOG_FILE
+    echo -e "$(date $LOGFILE_DATEFORMAT) $THEME_BOFLINE Docker Config Location: $DOCKER_LOCATION" >> $LOG_FILE
   fi
 fi
 
@@ -110,17 +112,19 @@ fi
 
 if [[ $CONTINUE == true && $INSTALL_CHOICES != "" ]]; then
   
-    ##{ # Start Progress Guage
+    { # Start Progress Guage
 
       # Install Step 1: Check to see if Docker is installed
       sleep 0.5
       echo -e "XXX\n0\nInstalling docker... \nXXX"
 
-      docker -v > DOCKER_INSTALLED
+      echo -ne "$(date $LOGFILE_DATEFORMAT) $THEME_BOFLINE Checking if Docker installed..." >> $LOG_FILE
+
+      DOCKER_INSTALLED="$(docker -v)" 
 
       if [[ $DOCKER_INSTALLED == *"not installed"* ]]; then
         # Install Docker
-        echo -e "$THEME_NOT_INSTALLED\n" >> $LOG_FILE
+        echo -e "$THEME_NOT_INSTALLED" >> $LOG_FILE
         echo -e "$THEME_BOFLINE Installing Docker...\n" >> $LOG_FILE
         sudo apt-get update && sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual && sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -128,64 +132,85 @@ if [[ $CONTINUE == true && $INSTALL_CHOICES != "" ]]; then
         sudo apt-get update
         sudo apt-get install docker
       else
-        echo -e "$THEME_INSTALLED\n" >> $LOG_FILE
+        echo -e "$THEME_INSTALLED" >> $LOG_FILE
       fi
 
       # Install Step 2: Stop any existing dockers
       sleep 0.5
       echo -e "XXX\n10\nStopping any existing dockers... \nXXX"
 
-      IFS=', ' read -r -a EXISTING_DOCKERS <<< "$EXISTING_DOCKERS"
-      for i in "${EXISTING_DOCKERS[@]}"
+      IFS=', ' read -r -a INSTALL_CHOICES <<< "$INSTALL_CHOICES"
+      for i in "${INSTALL_CHOICES[@]}"
       do
          # do whatever on $i
-         if [[ "$(docker inspect --format=\"{{.State.Running}}\" ${i,,} 2> /dev/null)" == true ]]; then
-            echo -ne "$(date +%Y-%m-%d:%H:%M:%S) $THEME_BOFLINE Stopping $i..."  >> $LOG_FILE
+         if [[ "$(sudo docker inspect --format=\"{{.State.Running}}\" ${i,,} 2> /dev/null)" == true ]]; then
+            echo -ne "$(date $LOGFILE_DATEFORMAT) $THEME_BOFLINE Stopping ${i,,}..."  >> $LOG_FILE
             sudo docker rm --force "${i,,}" > /dev/null
-            echo -e "$THEME_OK\n"
+            echo -e "$THEME_OK" >> $LOG_FILE
          fi
       done
 
-      ## Begin processing choices
-      #IFS=' ' read -r -a install_choices <<< "$INSTALL_CHOICES"
-      #for install_choice in "${INSTALL_CHOICES[@]}"
-      #do
+      # Check if the user wants to use Portainer
+      if [[ $VPN="yes" ]]; then
 
-      #done
+          sleep 0.5
+          echo -e "XXX\n20\nInstalling Portainer... \nXXX"
+          echo -ne "$(date $LOGFILE_DATEFORMAT) $THEME_BOFLINE Starting Portainer install..." >> $LOG_FILE
+
+          INIT_ERROR="$(sudo docker run -d  --name=portainer --restart=always -p 9000:9000  -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer 2>&1)"   
+          echo -e "$(date $LOGFILE_DATEFORMAT) $THEME_BOFLINE ERROR: $INIT_ERROR"  >> $LOG_FILE   
+
+          if [[ $INIT_ERROR == *"Error"* ]]; then
+             echo -e "$THEME_FAILED"  >> $LOG_FILE
+             echo -e "$THEME_BOFLINE ERROR: $INIT_ERROR"  >> $LOG_FILE
+          else
+             echo -e "$THEME_SUCCESS"
+          fi
+      fi
 
       # Check if the user wants to use a VPN
       if [[ $VPN="yes" ]]; then
-          echo -ne "$THEME_BOFLINE Starting VPN install..."
-          sudo docker run -d -it --restart=always --cap-add=NET_ADMIN --device /dev/net/tun --name vpn -v "$DOCKER_LOCATION"/openvpn:/vpn dperson/openvpn-client -fd > INIT_ERROR       
+
+          sleep 0.5
+          echo -e "XXX\n30\nInstalling VPN... \nXXX"
+          echo -ne "$(date $LOGFILE_DATEFORMAT) $THEME_BOFLINE Starting VPN install..." >> $LOG_FILE
+
+          INIT_ERROR="$(sudo docker run -d -it --restart=always --cap-add=NET_ADMIN --device /dev/net/tun --name vpn -v "$DOCKER_LOCATION"/openvpn:/vpn dperson/openvpn-client -fd 2>&1)"      
 
           if [[ $INIT_ERROR == *"Error"* ]]; then
-             echo -e "$THEME_FAILED"
-             echo -e "$THEME_BOFLINE ERROR: $INIT_ERROR"
+             echo -e "$THEME_FAILED"  >> $LOG_FILE
+             echo -e "$THEME_BOFLINE ERROR: $INIT_ERROR"  >> $LOG_FILE
           else
              echo -e "$THEME_SUCCESS"
           fi
       fi
       # Check if user wants to use Sonarr
       if [[ $INSTALL_CHOICES == *"Sonarr"* ]]; then
-         echo -ne "$THEME_BOFLINE Starting Sonarr install..."
-         if [[ $VPN == "yes" ]]; then
-            sudo docker run --restart=always -d --name=sonarr -p 8989:8989 -e PUID=1000 -e PGID=1000 -v /dev/rtc:/dev/rtc:ro -e TZ="Europe/London" -v "$DOCKER_LOCATION"/sonarr/config:/config -v "$TVSHOWS_LOCATION":/tv -v "$DOWNLOADS_LOCATION":/downloads -v "$DOWNLOADS_LOCATION":/NAS/downloads -v "$TVSHOWS_LOCATION":"$TVSHOWS_LOCATION" linuxserver/sonarr > INIT_ERROR      
+          sleep 0.5
+          echo -e "XXX\n40\nInstalling Sonarr... \nXXX"
+          echo -ne "$(date $LOGFILE_DATEFORMAT) $THEME_BOFLINE Starting Sonarr install..." >> $LOG_FILE
+
+          if [[ $VPN == "yes" ]]; then
+            INIT_ERROR="$(sudo docker run --restart=always -d --name=sonarr -p 8989:8989 -e PUID=1000 -e PGID=1000 -v /dev/rtc:/dev/rtc:ro -e TZ="Europe/London" -v "$DOCKER_LOCATION"/sonarr/config:/config -v "$TVSHOWS_LOCATION":/tv -v "$DOWNLOADS_LOCATION":/downloads -v "$DOWNLOADS_LOCATION":/NAS/downloads -v "$TVSHOWS_LOCATION":"$TVSHOWS_LOCATION" linuxserver/sonarr 2>&1)"      
           else
-           # Install docker including port numbers without using VPN (To-do)
-           echo "Unfinished code..."
+            # Install docker including port numbers without using VPN (To-do)
+            echo "Unfinished code..."
          fi
          if [[ $INIT_ERROR == *"Error"* ]]; then
-           echo -e "$THEME_FAILED"
-           echo echo -e "$THEME_BOFLINE ERROR: $INIT_ERROR"
+           echo -e "$THEME_FAILED" >> $LOG_FILE
+           echo echo -e "$THEME_BOFLINE ERROR: $INIT_ERROR" >> $LOG_FILE
          else
-            echo -e "$THEME_SUCCESS"
+            echo -e "$THEME_SUCCESS" >> $LOG_FILE
          fi
       fi
       # Check if user wants to use Radarr
-      if [[ $INSTALL_CHOICES == *"Sonarr"* ]]; then
-         echo -ne "$THEME_BOFLINE Starting Radarr install..."
+      if [[ $INSTALL_CHOICES == *"Radarr"* ]]; then
+          sleep 0.5
+          echo -e "XXX\n50\nInstalling Radarr... \nXXX"
+          echo -ne "$(date $LOGFILE_DATEFORMAT) $THEME_BOFLINE Starting Radarr install..." >> $LOG_FILE
+
          if [[ $VPN == "yes" ]]; then
-            sudo docker run -d --restart=always --name=radarr -v "$DOCKER_LOCATION"/radarr/config:/config -v "$DOWNLOADS_LOCATION":/downloads -v "$MOVIES_LOCATION":/movies -e PGID=1000 -e PUID=1000 -e TZ="Europe/London" -v /dev/rtc:/dev/rtc:ro -p 7878:7878 linuxserver/radarr > INIT_ERROR
+            INIT_ERROR="$(sudo docker run -d --restart=always --name=radarr -v "$DOCKER_LOCATION"/radarr/config:/config -v "$DOWNLOADS_LOCATION":/downloads -v "$MOVIES_LOCATION":/movies -e PGID=1000 -e PUID=1000 -e TZ="Europe/London" -v /dev/rtc:/dev/rtc:ro -p 7878:7878 linuxserver/radarr 2>&1)"
           else
            # Install docker including port numbers without using VPN (To-do)
            echo "Unfinished code..."
@@ -199,9 +224,11 @@ if [[ $CONTINUE == true && $INSTALL_CHOICES != "" ]]; then
       fi
       # Check if user wants to use Deluge
       if [[ $INSTALL_CHOICES == *"Deluge"* ]]; then
-         echo -ne "$THEME_BOFLINE Starting Deluge install..."
+         sleep 0.5
+          echo -e "XXX\n60\nInstalling Deluge... \nXXX"
+          echo -ne "$(date $LOGFILE_DATEFORMAT) $THEME_BOFLINE Starting Deluge install..." >> $LOG_FILE
          if [[ $VPN == "yes" ]]; then
-            sudo docker run -d --restart=always --name=deluge --net=container:vpn -e PUID=1000 -e PGID=1000 -e TZ=Europe/London -v "$DOWNLOADS_LOCATION":/downloads -v "$DOCKER_LOCATION"/deluge:/config linuxserver/deluge  > INIT_ERROR
+            INIT_ERROR="$(sudo docker run -d --restart=always --name=deluge --net=container:vpn -e PUID=1000 -e PGID=1000 -e TZ=Europe/London -v "$DOWNLOADS_LOCATION":/downloads -v "$DOCKER_LOCATION"/deluge:/config linuxserver/deluge 2>&1)"
   	     else
            # Install docker including port numbers without using VPN (To-do)
            echo "Unfinished code..."
@@ -215,9 +242,11 @@ if [[ $CONTINUE == true && $INSTALL_CHOICES != "" ]]; then
       fi
       # Check if the user wants to use Cardigann
       if [[ $INSTALL_CHOICES == *"Cardigann"* ]]; then
-         echo -ne "$THEME_BOFLINE Starting Cardigann install..."
+         sleep 0.5
+          echo -e "XXX\n70\nInstalling Cardigann... \nXXX"
+          echo -ne "$(date $LOGFILE_DATEFORMAT) $THEME_BOFLINE Starting Cardigann install..." >> $LOG_FILE
          if [[ $VPN == "yes" ]]; then
-           sudo docker run -d --restart=always --name=cardigann --net=container:vpn -v "$DOCKER_LOCATION"/cardigann:/config -e PGID=1000 -e PUID=1000 linuxserver/cardigann > INIT_ERROR
+           INIT_ERROR="$(sudo docker run -d --restart=always --name=cardigann --net=container:vpn -v "$DOCKER_LOCATION"/cardigann:/config -e PGID=1000 -e PUID=1000 linuxserver/cardigann 2>&1)"
          else
            # Install docker including port numbers without using VPN (To-do)
            echo "Unfinished code..."
@@ -232,9 +261,11 @@ if [[ $CONTINUE == true && $INSTALL_CHOICES != "" ]]; then
       fi
       # Check if the user wants to use Jackett
       if [[ $INSTALL_CHOICES == *"Jackett"* ]]; then
-         echo -ne "$THEME_BOFLINE Starting Jackett install..."
+         sleep 0.5
+          echo -e "XXX\n80\nInstalling Jackett... \nXXX"
+          echo -ne "$(date $LOGFILE_DATEFORMAT) $THEME_BOFLINE Starting Jackett install..." >> $LOG_FILE
          if [[ $VPN == "yes" ]]; then
-           INIT_ERROR="$(sudo docker run --restart=always -d --name=jackett --net=container:vpn -v /etc/localtime:/etc/localtime:ro -v "$DOCKER_LOCATION"/jackett:/config -v "$DOWNLOADS_LOCATION":/downloads -e PUID=1000 -e PGID=1000 -e TZ="Europe/London" linuxserver/jackett $QUIET_MODE)"
+           INIT_ERROR="$(sudo docker run --restart=always -d --name=jackett --net=container:vpn -v /etc/localtime:/etc/localtime:ro -v "$DOCKER_LOCATION"/jackett:/config -v "$DOWNLOADS_LOCATION":/downloads -e PUID=1000 -e PGID=1000 -e TZ="Europe/London" linuxserver/jackett 2>&1)"
          else
            # Install docker including port numbers without using VPN (To-do)
            echo "Unfinished code..."
@@ -249,8 +280,10 @@ if [[ $CONTINUE == true && $INSTALL_CHOICES != "" ]]; then
       fi
       # Install Proxy Server for accessing VPN INSTALL_CHOICES
       if [[ $VPN="yes" ]]; then
-          echo -ne "$THEME_BOFLINE Starting Proxy install..."
-          sudo docker run -it --name proxy -p 80:80 -p 443:443 -p 9117:9117 --link vpn:deluge --link vpn:jackett --link vpn:cardigann --restart=always -v "$DOCKER_LOCATION"/nginx/default.conf:/etc/nginx/conf.d/default.conf -d dperson/nginx > INIT_ERROR
+          sleep 0.5
+          echo -e "XXX\n90\nInstalling Nginx Proxy... \nXXX"
+          echo -ne "$(date $LOGFILE_DATEFORMAT) $THEME_BOFLINE Starting Nginx Proxy install..." >> $LOG_FILE
+          INIT_ERROR="$(sudo docker run -it --name proxy -p 80:80 -p 443:443 -p 9117:9117 --link vpn:deluge --link vpn:jackett --link vpn:cardigann --restart=always -v "$DOCKER_LOCATION"/nginx/default.conf:/etc/nginx/conf.d/default.conf -d dperson/nginx 2>&1)"
           if [[ $INIT_ERROR == *"Error"* ]]; then
              echo -e "$THEME_FAILED"
              echo -e "$THEME_BOFLINE ERROR: $INIT_ERROR"
@@ -258,7 +291,7 @@ if [[ $CONTINUE == true && $INSTALL_CHOICES != "" ]]; then
              echo -e "$THEME_SUCCESS"
           fi
       fi
-    ##} | whiptail --title "Installation" --gauge "Please wait while installing software..." 6 60 0
+    } | whiptail --title "Installation" --gauge "Please wait while installing software..." 6 60 0
 else
     echo -e "$THEME_BOFLINE Exiting without installing."
 fi
